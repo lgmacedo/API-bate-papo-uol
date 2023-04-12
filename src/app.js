7f23cd98c9f783372380d0f958ccefb0c2b8adf8
lgmacedo <lucasguerra.macedo@gmail.com>
2023-04-12 13:42:42 -0300

    POST/ participants and GET/ participants routes done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import Joi from "joi";
 import dayjs from "dayjs";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const participants = [];
 const messages = [];
@@ -10,28 +13,49 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+mongoClient
+  .connect()
+  .then(() => (db = mongoClient.db()))
+  .catch((err) => console.log(err.message));
+
 server.post("/participants", (req, res) => {
   const { name } = req.body;
-  const schema = Joi.string().empty();
+  const schema = Joi.string().required();
   if (schema.validate(name).error) {
     return res.sendStatus(422);
   }
-  if (participants.find((p) => p.name === name)) {
-    return res.sendStatus(409);
-  }
-  participants.push({ name, lastStatus: Date.now() });
-  messages.push({
-    from: name,
-    to: "Todos",
-    text: "entra na sala...",
-    type: "status",
-    time: dayjs().format("HH:mm:ss"),
-  });
-  res.sendStatus(201);
+  db.collection("participants")
+    .findOne({ name: name })
+    .then((user) => {
+      if (user !== null) {
+        res.sendStatus(409);
+      } else {
+        db.collection("participants")
+          .insertOne({ name: name, lastStatus: Date.now() })
+          .catch((err) => console.log(err.message));
+        db.collection("messages")
+          .insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: dayjs().format("HH:mm:ss"),
+          })
+          .catch((err) => console.log(err.message));
+        res.sendStatus(201);
+      }
+    })
+    .catch((err) => console.log(err.message));
 });
 
 server.get("/participants", (req, res) => {
-  res.send(participants);
+  db.collection("participants")
+    .find()
+    .toArray()
+    .then((users) => res.send(users))
+    .catch((err) => console.log(err.message));
 });
 
 server.post("/messages", (req, res) => {
@@ -56,15 +80,15 @@ server.post("/messages", (req, res) => {
 });
 
 server.get("/messages", (req, res) => {
-    const {User} = req.headers;
-    const {limit} = req.query;
+  const { User } = req.headers;
+  const { limit } = req.query;
 });
 
 server.post("/status", (req, res) => {
-    const {User} = req.headers;
-    if(!User){
-        return res.sendStatus(404);
-    }
+  const { User } = req.headers;
+  if (!User) {
+    return res.sendStatus(404);
+  }
 });
 
 const PORT = 5001;
