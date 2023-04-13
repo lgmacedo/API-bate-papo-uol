76ec4f8876170211dde8b287e54bbb53e4038be6
lgmacedo <lucasguerra.macedo@gmail.com>
2023-04-13 15:31:34 -0300

    more bug fixes

--- src/app.js
+++ src/app.js
@@ -135,7 +135,7 @@ server.post("/status", (req, res) => {
       } else {
         db.collection("participants")
           .updateOne({ name: user.name }, { $set: { lastStatus: Date.now() } })
-          .then(res.sendStatus(200))
+          .then(res.sendStatus(201))
           .catch((err) => console.log(err.message));
       }
     })
